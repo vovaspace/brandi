d3faec66d48fc59e4fabaed95865f293779b7819
Vladimir Lewandowski <vova.space@icloud.com>
2021-02-15 16:51:44 +0400

    Adds: autotests

---
+++ spec/injected.ts
@@ -0,0 +1,52 @@
+import { injected, token } from '../src';
+import { injectsRegistry } from '../src/globals';
+
+describe('injected', () => {
+  beforeAll(() => {
+    injectsRegistry.clear();
+  });
+
+  afterEach(() => {
+    injectsRegistry.clear();
+  });
+
+  it("adds a single token to 'injectsRegistry'", () => {
+    const someToken = token<string>('some');
+    class SomeClass {
+      constructor(public str: string) {}
+    }
+
+    injected(SomeClass, someToken);
+
+    expect(injectsRegistry.get(SomeClass)).toStrictEqual([someToken]);
+  });
+
+  it("adds multiple tokens to 'injectsRegistry'", () => {
+    const tokens = {
+      first: token<string>('first'),
+      second: token<number>('second'),
+    };
+    class SomeClass {
+      constructor(public str: string, public num: number) {}
+    }
+
+    injected(SomeClass, tokens.first, tokens.second);
+
+    expect(injectsRegistry.get(SomeClass)).toStrictEqual([tokens.first, tokens.second]);
+  });
+
+  it('rewrites tokens', () => {
+    const tokens = {
+      first: token<string>('first'),
+      second: token<string>('second'),
+    };
+    class SomeClass {
+      constructor(public str: string) {}
+    }
+
+    injected(SomeClass, tokens.first);
+    injected(SomeClass, tokens.second);
+
+    expect(injectsRegistry.get(SomeClass)).toStrictEqual([tokens.second]);
+  });
+});
