67fd1832723a7017fce739adfc0184c1664834f8
Vladimir Lewandowski <vova.space@icloud.com>
2021-04-18 11:55:05 +0300

    Removes: `React.useContext` from `ContainerProvider`; Reorganizes: spec

--- packages/brandi-react/spec/container.spec.tsx
+++ packages/brandi-react/spec/container.spec.tsx
@@ -6,92 +6,102 @@ import { ContainerProvider } from '../src';
 import { useContainer } from '../src/container';
 
 describe('container', () => {
-  it("passes a container clone through 'ContainerProvider'", () => {
-    const tokens = {
-      some: token<number>('some'),
-    };
+  describe('useContainer', () => {
+    it("throws error when a container is not passed through 'ContainerProvider'", () => {
+      const { result } = renderHook(() => useContainer());
 
-    const someValue = 1;
-    const anotherValue = 2;
+      expect(result.error).toBeInstanceOf(Error);
+      expect(result.error).toMatchSnapshot();
+    });
 
-    const container = createContainer();
-    container.bind(tokens.some).toConstant(someValue);
+    it("returns 'null' in not strict mode when a container is not passed through 'ContainerProvider'", () => {
+      const { result } = renderHook(() => useContainer(false));
 
-    const wrapper: React.FunctionComponent = ({ children }) => (
-      <ContainerProvider container={container}>{children}</ContainerProvider>
-    );
-
-    const { result } = renderHook(() => useContainer(), { wrapper });
-
-    container.bind(tokens.some).toConstant(anotherValue);
-
-    expect(result.current).not.toBe(container);
-    expect(result.current).toBeInstanceOf(Container);
-    expect(result.current.get(tokens.some)).toBe(someValue);
+      expect(result.current).toBeNull();
+    });
   });
 
-  it('binds the parent container from the parent context', () => {
-    const tokens = {
-      some: token<number>('some'),
-      another: token<number>('another'),
-    };
+  describe('ContainerProvider', () => {
+    it('passes a container clone', () => {
+      const tokens = {
+        some: token<number>('some'),
+      };
 
-    const someValue = 1;
-    const anotherValue = 2;
+      const someValue = 1;
+      const anotherValue = 2;
 
-    const parentContainer = createContainer();
-    parentContainer.bind(tokens.some).toConstant(someValue);
+      const container = createContainer();
+      container.bind(tokens.some).toConstant(someValue);
 
-    const childContainer = createContainer();
-    childContainer.bind(tokens.another).toConstant(anotherValue);
+      const wrapper: React.FunctionComponent = ({ children }) => (
+        <ContainerProvider container={container}>{children}</ContainerProvider>
+      );
 
-    const wrapper: React.FunctionComponent = ({ children }) => (
-      <ContainerProvider container={parentContainer}>
-        <ContainerProvider container={childContainer}>
-          {children}
+      const { result } = renderHook(() => useContainer(), { wrapper });
+
+      container.bind(tokens.some).toConstant(anotherValue);
+
+      expect(result.current).not.toBe(container);
+      expect(result.current).toBeInstanceOf(Container);
+      expect(result.current.get(tokens.some)).toBe(someValue);
+    });
+
+    it('binds the parent container from the parent context', () => {
+      const tokens = {
+        some: token<number>('some'),
+        another: token<number>('another'),
+      };
+
+      const someValue = 1;
+      const anotherValue = 2;
+
+      const parentContainer = createContainer();
+      parentContainer.bind(tokens.some).toConstant(someValue);
+
+      const childContainer = createContainer();
+      childContainer.bind(tokens.another).toConstant(anotherValue);
+
+      const wrapper: React.FunctionComponent = ({ children }) => (
+        <ContainerProvider container={parentContainer}>
+          <ContainerProvider container={childContainer}>
+            {children}
+          </ContainerProvider>
         </ContainerProvider>
-      </ContainerProvider>
-    );
+      );
 
-    const { result } = renderHook(() => useContainer(), { wrapper });
+      const { result } = renderHook(() => useContainer(), { wrapper });
 
-    expect(result.current.get(tokens.some)).toBe(someValue);
-    expect(result.current.get(tokens.another)).toBe(anotherValue);
-  });
+      expect(result.current.get(tokens.some)).toBe(someValue);
+      expect(result.current.get(tokens.another)).toBe(anotherValue);
+    });
 
-  it("keeps the original parent container with 'isolated' prop", () => {
-    const tokens = {
-      some: token<number>('some'),
-    };
+    it("keeps the original parent container with 'isolated' prop", () => {
+      const tokens = {
+        some: token<number>('some'),
+      };
 
-    const someValue = 1;
-    const anotherValue = 2;
+      const someValue = 1;
+      const anotherValue = 2;
 
-    const parentContainer = createContainer();
-    parentContainer.bind(tokens.some).toConstant(someValue);
+      const parentContainer = createContainer();
+      parentContainer.bind(tokens.some).toConstant(someValue);
 
-    const anotherParentContainer = createContainer();
-    anotherParentContainer.bind(tokens.some).toConstant(anotherValue);
+      const anotherParentContainer = createContainer();
+      anotherParentContainer.bind(tokens.some).toConstant(anotherValue);
 
-    const childContainer = createContainer(parentContainer);
+      const childContainer = createContainer(parentContainer);
 
-    const wrapper: React.FunctionComponent = ({ children }) => (
-      <ContainerProvider container={anotherParentContainer}>
-        <ContainerProvider container={childContainer} isolated>
-          {children}
+      const wrapper: React.FunctionComponent = ({ children }) => (
+        <ContainerProvider container={anotherParentContainer}>
+          <ContainerProvider container={childContainer} isolated>
+            {children}
+          </ContainerProvider>
         </ContainerProvider>
-      </ContainerProvider>
-    );
+      );
 
-    const { result } = renderHook(() => useContainer(), { wrapper });
+      const { result } = renderHook(() => useContainer(), { wrapper });
 
-    expect(result.current.get(tokens.some)).toBe(someValue);
-  });
-
-  it("throws error when a container is not passed through 'ContainerProvider'", () => {
-    const { result } = renderHook(() => useContainer());
-
-    expect(result.error).toBeInstanceOf(Error);
-    expect(result.error).toMatchSnapshot();
+      expect(result.current.get(tokens.some)).toBe(someValue);
+    });
   });
 });
--- packages/brandi-react/src/container/ContainerProvider.tsx
+++ packages/brandi-react/src/container/ContainerProvider.tsx
@@ -2,12 +2,13 @@ import { Container } from 'brandi';
 import React from 'react';
 
 import { ContainerContext } from './ContainerContext';
+import { useContainer } from './useContainer';
 
 export const ContainerProvider: React.FunctionComponent<{
   container: Container;
   isolated?: boolean;
 }> = ({ children, container, isolated }) => {
-  const parentContainer = React.useContext(ContainerContext);
+  const parentContainer = useContainer(false);
   const clonedContainer = React.useMemo(() => container.clone(), [container]);
 
   if (!isolated && parentContainer !== null) {
