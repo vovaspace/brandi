1196f4c3660c665bccc543cbc6306ea13144aa41
Vladimir Lewandowski <vova.space@icloud.com>
2021-04-18 12:19:31 +0300

    Adds: `React.useMemo` to `useInjection`

--- packages/brandi-react/spec/injection.spec.tsx
+++ packages/brandi-react/spec/injection.spec.tsx
@@ -82,6 +82,67 @@ describe('injection', () => {
 
       expect(result.current).toBeUndefined();
     });
+
+    it('keeps the same transient scoped dependency between renderers', () => {
+      class Some {}
+
+      const tokens = {
+        some: token<Some>('some'),
+      };
+
+      const container = createContainer();
+      container.bind(tokens.some).toInstance(Some).inTransientScope();
+
+      const wrapper: React.FunctionComponent = ({ children }) => (
+        <ContainerProvider container={container}>{children}</ContainerProvider>
+      );
+
+      const { result, rerender } = renderHook(
+        () => useInjection(tokens.some.optional),
+        {
+          wrapper,
+        },
+      );
+
+      rerender();
+
+      expect(result.all[0]).toBeInstanceOf(Some);
+      expect(result.all[1]).toBeInstanceOf(Some);
+      expect(result.all[0]).toBe(result.all[1]);
+    });
+
+    it('returns individual transient scoped dependencies for separate renders', () => {
+      class Some {}
+
+      const tokens = {
+        some: token<Some>('some'),
+      };
+
+      const container = createContainer();
+      container.bind(tokens.some).toInstance(Some).inTransientScope();
+
+      const wrapper: React.FunctionComponent = ({ children }) => (
+        <ContainerProvider container={container}>{children}</ContainerProvider>
+      );
+
+      const { result: firstResult } = renderHook(
+        () => useInjection(tokens.some.optional),
+        {
+          wrapper,
+        },
+      );
+
+      const { result: secondResult } = renderHook(
+        () => useInjection(tokens.some.optional),
+        {
+          wrapper,
+        },
+      );
+
+      expect(firstResult.current).toBeInstanceOf(Some);
+      expect(secondResult.current).toBeInstanceOf(Some);
+      expect(firstResult.current).not.toBe(secondResult.current);
+    });
   });
 
   describe('createInjectionHooks', () => {
--- packages/brandi-react/src/injection/useInjection.ts
+++ packages/brandi-react/src/injection/useInjection.ts
@@ -1,7 +1,16 @@
 import { TokenType, TokenValue } from 'brandi';
+import React from 'react';
 
 import { useConditions } from '../conditions';
 import { useContainer } from '../container';
 
-export const useInjection = <T extends TokenValue>(token: T): TokenType<T> =>
-  useContainer().get(token, useConditions());
+export const useInjection = <T extends TokenValue>(token: T): TokenType<T> => {
+  const container = useContainer();
+  const conditions = useConditions();
+
+  return React.useMemo(() => container.get(token, conditions), [
+    token,
+    conditions,
+    container,
+  ]);
+};
