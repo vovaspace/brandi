8e035b74ed9aadaf3a4b0edabac7270843ffc7f1
Vladimir Lewandowski <vova.space@icloud.com>
2021-04-28 20:18:11 +0300

    Adds: `isolated` default `false` value to `ContainerProvider`

--- packages/brandi-react/src/container/ContainerProvider.tsx
+++ packages/brandi-react/src/container/ContainerProvider.tsx
@@ -7,7 +7,7 @@ import { useContainer } from './useContainer';
 export const ContainerProvider: React.FunctionComponent<{
   container: Container;
   isolated?: boolean;
-}> = ({ children, container, isolated }) => {
+}> = ({ children, container, isolated = false }) => {
   const parentContainer = useContainer(false);
   const clonedContainer = React.useMemo(() => container.clone(), [container]);
 
