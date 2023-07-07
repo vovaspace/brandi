7f7bcd5486e07bde3497c1525be49075bc4e9b64
Yanis Urbis <yanis.urbis@gmail.com>
2023-07-07 13:07:33 +0400

    create a test that highlight a problem
    the error is confusing and unhelpful

---
+++ packages/brandi/spec/binding-error.spec.ts
@@ -0,0 +1,40 @@
+import { Container, injected, token } from '../src';
+
+describe('common container error', () => {
+  const createLoggerModule = () => {
+    throw new Error('error to stop logger module from being created')
+    return {
+      log: (message: string) => {
+        // eslint-disable-next-line no-console
+        console.log(message)
+      },
+    }
+  }
+  type Logger = ReturnType<typeof createLoggerModule>
+
+  const createAdderModule = (logger: Logger) => {
+    return {
+      add: (a: number, b: number) => {
+        logger.log('adding')
+        return a + b
+      },
+    }
+  }
+  type Adder = ReturnType<typeof createAdderModule>
+
+  const TOKENS = {
+    logger: token<Logger>('logger'),
+    adder: token<Adder>('adder'),
+  }
+
+  const container = new Container()
+  injected(createAdderModule, TOKENS.logger)
+  container.bind(TOKENS.adder).toInstance(createAdderModule).inSingletonScope()
+  container.bind(TOKENS.logger).toInstance(createLoggerModule).inSingletonScope()
+
+  it('fail to retrieve a module with understandable error', () => {
+    expect(() => {
+      container.get(TOKENS.adder)
+    }).toThrow(new TypeError('creator is not a constructor'))
+  })
+})
